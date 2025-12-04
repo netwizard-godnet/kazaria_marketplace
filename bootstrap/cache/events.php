fab37bde224cb5b592d528569e34063b92dbb891
netwizard-godnet <72748482+netwizard-godnet@users.noreply.github.com>
2025-12-04 14:15:21 +0000

    Cache

--- bootstrap/cache/events.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'Illuminate\\Foundation\\Support\\Providers\\EventServiceProvider' => 
-  array (
-    'Illuminate\\Foundation\\Events\\CSRFValidationError' => 
-    array (
-      0 => 'App\\Listeners\\CsrfErrorListener@handle',
-    ),
-  ),
-);
