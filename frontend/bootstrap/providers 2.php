dd8d1f02376abfc5229c36a1d0e4016e5428b539
Israel Anako <73380263+killlinux-cell@users.noreply.github.com>
2026-01-06 10:45:23 +0000

    fonctionne bien

---
+++ frontend/bootstrap/providers 2.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
+    App\Providers\SettingServiceProvider::class,
+    Barryvdh\DomPDF\ServiceProvider::class,
+];
